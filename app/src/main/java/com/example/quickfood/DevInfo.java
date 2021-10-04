8d8485be8784bbba5114038515d574cc116b780a
JonasEbid <223322@usn.no>
2021-10-04 02:04:14 +0200

    removed git test

--- app/src/main/java/com/example/quickfood/DevInfo.java
+++ app/src/main/java/com/example/quickfood/DevInfo.java
@@ -9,7 +9,6 @@ public class DevInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_devinfo);
-        // test Git
     }
 }
 
